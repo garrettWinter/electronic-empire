78007dadcc04c7c570db5001033005cef7368e28
Garrett Winter <Garrett_Winter@hotmail.com>
2023-10-02 16:01:07 -0500

    Setup authentication, session and api routes to create users and login.

---
+++ app/api/login/route.ts
@@ -0,0 +1,26 @@
+import prisma from '@/app/lib/prisma';
+
+import * as bcrypt from 'bcrypt';
+
+interface RequestBody {
+    username: string;
+    password: string;
+}
+export async function POST(request: Request) {
+    console.log("login route has run")
+    const body: RequestBody = await request.json();
+    console.log(body)
+    const user = await prisma.user.findUnique({
+        where: {
+            username: body.username,
+        }
+    })
+    console.log(user);
+    if (user && (await bcrypt.compare(body.password, user.password))) {
+        const { password, ...userWithoutPass } = user
+        console.log(password);
+        console.log(userWithoutPass);
+        return new Response(JSON.stringify(userWithoutPass));
+    } else return new Response(JSON.stringify(null))
+
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Header from "./components/Header";
+import Providers from './components/Providers';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,8 +20,10 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <div>
+        <Providers>
         <Header />
         {children}
+        </Providers>
         </div>
         </body>
     </html>
