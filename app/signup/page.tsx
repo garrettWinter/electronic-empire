a213c48a4d5bfe9494544a7fb064a3df526fb061
Garrett Winter <Garrett_Winter@hotmail.com>
2023-11-06 14:23:38 -0600

    Created a Signup page, that will create a new user, and then if it gets a ok response from the API will then login the user. I also left some comments for future development.

---
+++ app/signup/page.tsx
@@ -0,0 +1,61 @@
+'use client'
+
+import styles from '../page.module.css'
+import { signIn } from "next-auth/react";
+import React, { useRef } from "react";
+
+export default function Signup() {
+  const username = useRef("");
+  const pass = useRef("");
+  let errorMessage = "";
+
+  const onSubmit = async () => {
+    //FUTURE DEVELOPMENT --- CHECK TO SEE IF USER ID IS USED ALREADY
+
+    //CREATE THE USER
+    const domain = window.location.origin;
+    const fetchURL = `${domain}/api/user`;
+    const res = await fetch(fetchURL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username.current,
+        password: pass.current,
+      }),
+    });
+    console.log(res);
+    console.log(res.ok);
+    if (res?.ok === true) {
+      console.log('SignUp Response OK, logging in user!');
+      // LOGIN THE USER
+      const result = await signIn("credentials", {
+        username: username.current,
+        password: pass.current,
+        redirect: true,
+        callbackUrl: "/"
+      });
+    } else {
+      //FUTURE DEVELOPMENT --- Add Error Messaging Componentw!!!
+    }
+  }
+
+  return (
+    <main className={styles.main}>
+      <h1>Account Creation</h1>
+      <div>
+        <div>
+          <p>User Name:</p>
+          <textarea name="text" placeholder='username' onChange={(e) => (username.current = e.target.value)}></textarea>
+        </div>
+        <div>
+          <p>Password:</p>
+          <input name="password" type="password" placeholder='password' onChange={(e) => (pass.current = e.target.value)}></input>
+        </div>
+        <button style={{ backgroundColor: "green" }} onClick={onSubmit}>Login</button>
+        <p>If you have an account already you can <a href="/login" style={{ color: "blue", textDecoration: "underline" }}>click here </a>to login.</p>
+      </div>
+    </main>
+  )
+}
