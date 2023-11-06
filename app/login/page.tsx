b315011a66dac384d4c456d49cd77d0e33b6a105
Garrett Winter <Garrett_Winter@hotmail.com>
2023-11-06 14:22:02 -0600

    Fixed the formating of the login/page.tsx file.

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,35 +5,35 @@ import { signIn } from "next-auth/react";
 import React, { useRef } from "react";
 
 export default function Login() {
-const username = useRef("");
-const pass = useRef("");
+  const username = useRef("");
+  const pass = useRef("");
 
-const onSubmit = async () => {
-  const result = await signIn("credentials", {
-username: username.current,
-password: pass.current,
-redirect: true,
-callbackUrl: "/"
-  });
-}
+  const onSubmit = async () => {
+    const result = await signIn("credentials", {
+      username: username.current,
+      password: pass.current,
+      redirect: true,
+      callbackUrl: "/"
+    });
+  }
 
   return (
     <main className={styles.main}>
-   <h1>Account Login</h1>
-   <div>
-    <div>
-      <p>User Name:</p>
-      <textarea name="text" placeholder='username' onChange={(e) => (username.current = e.target.value)}></textarea>
-    </div>
-    <div>
-      <p>Password:</p>
-      <input name="password" type="password" placeholder='password' onChange={(e) => (pass.current = e.target.value)}></input>
-    </div>
-    <button style={{backgroundColor: "green"}}onClick={onSubmit}>Login</button>
-    <p>If you dont have an account you can <a href="/signup">click here </a>to create one.</p>
-    <br></br>
-    <p> test1:123123 and test2:123123 are current test accounts (username:password).</p>
-   </div>
+      <h1>Account Login</h1>
+      <div>
+        <div>
+          <p>User Name:</p>
+          <textarea name="text" placeholder='username' onChange={(e) => (username.current = e.target.value)}></textarea>
+        </div>
+        <div>
+          <p>Password:</p>
+          <input name="password" type="password" placeholder='password' onChange={(e) => (pass.current = e.target.value)}></input>
+        </div>
+        <button style={{ backgroundColor: "green" }} onClick={onSubmit}>Login</button>
+        <p>If you dont have an account you can <a href="/signup" style={{ color: "blue", textDecoration: "underline" }}>click here </a>to create one.</p>
+        <br></br>
+        <p> test1:123123 and test2:123123 are current test accounts (username:password).</p>
+      </div>
     </main>
   )
 }
