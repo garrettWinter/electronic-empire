57c2803d83d0b7a3cb5b617f48728f1b12c8c725
Garrett Winter <Garrett_Winter@hotmail.com>
2023-11-07 13:26:40 -0600

    HeroProducts component is now pulling the first 4 records from the catalog and then mapping on these to create the hero products on the home page. Adjusted images so these were pulled from a local file, rather hosted on a external site.

--- app/components/HeroBanner.tsx
+++ app/components/HeroBanner.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default async function HeroBanner() {
     return (
         <div style={{ maxWidth: '100%', maxHeight: '500px' }}>
-            <img src="/images/banner-5250183_1280.jpg" alt="This is the alt tag for the banner."></img>
+            <img src="/images/banner-5250183_1280.jpg" alt="This is the alt tag for the banner." title="This is the alt tag for the banner."></img>
         </div>
     )
 };
--- app/components/HeroProducts.tsx
+++ app/components/HeroProducts.tsx
@@ -1,20 +1,35 @@
 import React from "react";
+import prisma from '../lib/prisma';
+import { wrap } from "module";
 
 export default async function HeroProducts() {
+    let heroProducts = await prisma.product.findMany({
+        take: 4
+    });
+    console.log(heroProducts);
     return (
-        <div style={{display:"flex", flexDirection:"row", paddingTop:20}}>
-
-            <div style={{display:"flex", flexDirection:"column", border: 'solid 2px', margin: 10, padding:10}}>
-                <p style={{ fontSize: 20, fontWeight: 'bolder' }}>Product Name</p>
-                <div style={{display:"flex", flexDirection:"row"}}>
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZ6cmDR54koroe_CWPYdoeLK8sXSSh7TwDLoleOXVjWA&s"></img>
-                    <div style={{display:"flex", flexDirection:"column", justifyContent: "space-around"}}>
-                        <p style={{ fontSize: 16, fontWeight: 'bold' }}>Price: $X,XXX.XX</p>
-                        <button style={{ backgroundColor: "green", color:"white" }}>Add to Cart</button>
+        <div style={{ display: "flex", flexDirection: "row", flexWrap: 'wrap', width: '33%', justifyContent: 'center'}}>
+            {heroProducts.map((product) => (
+                <div style={{ display: "flex", flexDirection: "row", paddingTop: 20 }}>
+                    <div style={{ display: "flex", flexDirection: "column", border: 'solid 2px', margin: 10, padding: 10 }}>
+                        <p style={{ fontSize: 20, fontWeight: 'bolder' }}>{product.productName}</p>
+                        <div style={{ display: "flex", flexDirection: "row" }}>
+                            <img
+                                title={product.productName}
+                                alt={product.productName}
+                                src={product.productImage}></img>
+                            <div style={{ display: "flex", flexDirection: "column", justifyContent: "space-around" }}>
+                                <p style={{ fontSize: 16, fontWeight: 'bold' }}>Price: {new Intl.NumberFormat('en-US', {
+                                    style: 'currency',
+                                    currency: 'USD'
+                                }).format(product.productPrice.toNumber())}</p>
+                                <button style={{ backgroundColor: "green", color: "white" }}>Add to Cart</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-
+            )
+            )}
         </div>
     )
 };
