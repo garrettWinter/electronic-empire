c50a18c654ba92fee4a747cf8beaca72f095b865
Garrett Winter <Garrett_Winter@hotmail.com>
2023-11-07 16:46:42 -0600

    Added a footer component and added it to the layout file. Also adjusted the viewheight of the main window to prevent scrolling bar on footer.

---
+++ app/components/Footer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default async function Footer(){
+
+    return(
+        <footer style={{
+            width: "100%",
+            backgroundColor: "transparent",
+            position: "static",
+            bottom: 0,
+            display: 'flex',
+            justifyContent: 'center'
+        }}>No rights resevered, developed in Q4 of 2023.</footer>
+    )
+};
