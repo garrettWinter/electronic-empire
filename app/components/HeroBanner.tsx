b569f045959a42a46a1c601186a0df9aa52ccb0e
Garrett Winter <Garrett_Winter@hotmail.com>
2023-11-07 13:49:18 -0600

    Create components for SpotLightBanner 1 & 2. Loaded placeholder banners in the image directory. Removed unneeded styling from the HeroBanner.

--- app/components/HeroBanner.tsx
+++ app/components/HeroBanner.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default async function HeroBanner() {
     return (
-        <div style={{ maxWidth: '100%', maxHeight: '500px' }}>
+        <div>
             <img src="/images/banner-5250183_1280.jpg" alt="This is the alt tag for the banner." title="This is the alt tag for the banner."></img>
         </div>
     )
--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,17 @@ import styles from './page.module.css';
 import React from 'react';
 import HeroBanner from './components/HeroBanner';
 import HeroProducts from './components/HeroProducts';
+import SpotLightBanner1 from './components/SpotLightBanner1';
+import SpotLightBanner2 from './components/SpotLightBanner2';
 
 export default function Home() {
   return (
     <main className={styles.main}>
     <p>Home Page</p>
     <HeroBanner/>
+    <SpotLightBanner1/>
     <HeroProducts/>
+    <SpotLightBanner2/>
   </main>
   )
 }
