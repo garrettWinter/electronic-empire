7c383ddd8740b18055b17d3f10cb70f35830b750
Garrett Winter <Garrett_Winter@hotmail.com>
2023-11-18 20:42:36 -0600

    Refactored the ProductDetails component to work better with dynamic routes. Added error handing for when a product is not found in the search, and created a NoProductFound component that is then displayed when error handling is triggered.

---
+++ app/components/NoProductFound.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default async function NoProductFound() {
+    return (
+        <div>
+            <h1>Sorry, we were unable to find a product with that ID!</h1>
+            <p>To see all products you can click here.</p>
+            <p>Or you can search for another product</p>
+            <p>ADD THE SEARCH BAR COMPONENT HERE </p>
+        </div>
+    )
+}
--- app/components/ProductDetails.tsx
+++ app/components/ProductDetails.tsx
@@ -1,50 +1,49 @@
 import React from "react";
 import prisma from '../lib/prisma';
 import Image from 'next/image';
+import NoProductFound from './NoProductFound';
 
 
-export default async function ProductDetails(params: {productId: number}) {
-    let product = params ? parseInt(params.productId.toString(), 10) : null;
-    //UPDATE SO CHECKS TO MAKE SURE CAN FIND RESULT AS WELL!!!!
-    console.log("In Product Details, reiceve param is "+params.productId);
-    if (product === null || product === undefined) {
-        return (
-            <div>No id query string present... Make it so this is the general all products page component.</div>
-        )
-    } else {
+export default async function ProductDetails(params: { productId: number }) {
+    let product = parseInt(params.productId.toString(), 10);
         const productDetails = await prisma.product.findUnique({
             where: {
                 productId: (product),
             },
         });
         console.log(productDetails);
-
-        return (
-            <div>
-                <div style={{ display: 'flex', flexDirection: 'row' }}>
-                    <Image
-                        title={productDetails!.productName}
-                        alt={productDetails!.productName}
-                        src={productDetails!.productImage}
-                        width='200'
-                        height='200'>
-                    </Image>
-                    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly', padding: '5px' }}>
-                        <p style={{ fontSize: '25px', fontWeight: 'bold' }}>{productDetails!.productName}</p>
-                        <p style={{ fontSize: '16px', fontWeight: 'bold' }}>Price: {new Intl.NumberFormat('en-US', {
-                            style: 'currency',
-                            currency: 'USD'
-                        }).format(productDetails!.productPrice.toNumber())}</p>
-                        <div style={{ display: 'flex', flexDirection: 'row' }}>
-                            <p>QTY:</p>
-                            <input placeholder='1' style={{ width: '25px' }}></input>
-                            <button style={{ backgroundColor: 'green', color: 'white', padding: '3px' }}>BUY NOW</button>
+        //If no product was found via the query, this will display the user the NoProductFound component.
+        if (productDetails === null) {
+            return (
+                <NoProductFound />
+            )
+        } else {
+            return (
+                <div>
+                    <div style={{ display: 'flex', flexDirection: 'row' }}>
+                        <Image
+                            title={productDetails!.productName}
+                            alt={productDetails!.productName}
+                            src={productDetails!.productImage}
+                            width='200'
+                            height='200'>
+                        </Image>
+                        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly', padding: '5px' }}>
+                            <p style={{ fontSize: '25px', fontWeight: 'bold' }}>{productDetails!.productName}</p>
+                            <p style={{ fontSize: '16px', fontWeight: 'bold' }}>Price: {new Intl.NumberFormat('en-US', {
+                                style: 'currency',
+                                currency: 'USD'
+                            }).format(productDetails!.productPrice.toNumber())}</p>
+                            <div style={{ display: 'flex', flexDirection: 'row' }}>
+                                <p>QTY:</p>
+                                <input placeholder='1' style={{ width: '25px' }}></input>
+                                <button style={{ backgroundColor: 'green', color: 'white', padding: '3px' }}>BUY NOW</button>
+                            </div>
                         </div>
                     </div>
+                    <p style={{ fontSize: '25px', fontWeight: 'bold' }}>Product Information:</p>
+                    <p>{productDetails?.ProductDescription}</p>
                 </div>
-                <p style={{ fontSize: '25px', fontWeight: 'bold' }}>Product Information:</p>
-                <p>{productDetails?.ProductDescription}</p>
-            </div>
-        )
-    }
-};
+            )
+        }
+    };
