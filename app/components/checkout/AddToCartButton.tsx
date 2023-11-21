9818aef765b3dfa763fa632a098bb9bedb0a7843
Garrett Winter <Garrett_Winter@hotmail.com>
2023-11-21 14:03:35 -0600

    Enhanced AddToCartButton so will not add duplicate products, and if product already exists, will update that cart item to incoming qty value.

--- app/components/checkout/AddToCartButton.tsx
+++ app/components/checkout/AddToCartButton.tsx
@@ -3,17 +3,30 @@ import React from "react";
 
 export default function AddToCartButton(parms: { productId: number, qty: number }) {
     function AddToCart() {
-         let product = {
+        let product = {
             productId: parms.productId,
             qty: parms.qty
         }
         if (localStorage.cart === null || localStorage.cart === undefined) {
             let cart = [product];
+            console.log("new Cart created")
             localStorage.setItem('cart', JSON.stringify(cart));
         } else {
             let cart = JSON.parse(localStorage.cart);
-            cart.push(product);
-            localStorage.setItem('cart', JSON.stringify(cart));
+            let search = cart.findIndex(( search: { productId: number }) => search.productId === product.productId);
+            if (search === -1) {
+                //Product not present in Cart, adding to cart
+                console.log("Product added to cart");
+                cart.push(product);
+                localStorage.setItem('cart', JSON.stringify(cart));
+                return;
+
+            } else {
+                console.log("Product in cart already, updating to incoming qty.");
+                cart[search].qty = product.qty;
+                localStorage.setItem('cart', JSON.stringify(cart));
+                return;
+            }
         }
     }
     return (
