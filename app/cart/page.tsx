e4ac3faa4f324c5c619ce2cb8f069ac7fb68f401
Garrett Winter <Garrett_Winter@hotmail.com>
2023-11-22 22:07:04 -0600

    Created a basic outline of the CartProductCard Component, and updated the cart to use this and look more like the mockup.

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,9 +1,15 @@
-import styles from '../page.module.css'
+import styles from '../page.module.css';
+import CartProductCard from '../components/checkout/CartProductCard';
 
-export default function Contact() {
+export default function Cart() {
   return (
     <main className={styles.main}>
       <p>Shopping Cart</p>
+      <CartProductCard />
+      <div>
+        <p>Order Total: $XXX.XX</p>
+        <button> Complete Order Button</button>
+      </div>
     </main>
   )
 }
---
+++ app/components/checkout/CartProductCard.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Image from 'next/image';
+import PlaceHolderImage from '../../../public/images/200x200-Product-Placeholder.jpg';
+
+export default async function CartProductCard() {
+    return (
+        <div style={{
+            display: 'flex'
+        }}>
+            <div style={{
+                display: 'flex',
+                flexDirection: 'row'
+            }}>
+                <Image
+                    // title={product.productName}
+                    alt='{product.productName}'
+                    width='200'
+                    height='200'
+                    src={PlaceHolderImage}>
+                </Image>
+                <div >
+                    <div style={{
+                        display: 'flex',
+                        flexDirection: 'row'
+                    }}>
+                        <button>Delete</button>
+                        <p>Product Name</p>
+                    </div>
+                    <p>Price: $XX,XXX.XX</p>
+                    <div style={{
+                        display: 'flex',
+                        flexDirection: 'row'
+                    }}>
+                        <p>QTY:</p>
+                        <input></input>
+                    </div>
+                    <p>Line Total: $X,XXX.XX</p>
+                </div>
+            </div>
+        </div>
+    )
+};
