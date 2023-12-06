51f23030304955fb1d166adb537652438955d4f3
Garrett Winter <Garrett_Winter@hotmail.com>
2023-12-06 13:03:39 -0600

    Added a error handling flow for when a order is attempted to be placed but somehow there are no more line items in the cart object.

--- app/components/checkout/CompleteOrderForm.tsx
+++ app/components/checkout/CompleteOrderForm.tsx
@@ -17,15 +17,19 @@ export default function CompleteOrderForm() {
         const cart = JSON.parse(localStorage.getItem("cart")!); //Overrid a Type Script warning about null, but this cannot be seen if cart is null.
 
         try {
+            if (cart === null || cart === undefined) {
+                window.alert("Error, we dont see any products in your cart anymore, and therefore we are unable to submit your order.");
+                return
+            }
             const result = await completeOrderAction(session.user.accessToken, cart);
             console.log("Order submitted successfully:\n", result);
             if (!result.orderId) {
                 window.alert("There was an issue, and we were unable to submit order.");
                 return
             } else {
-                window.alert("Your order has been placed. The Order ID is: "+result.orderId);
+                window.alert("Your order has been placed. The Order ID is: " + result.orderId);
                 localStorage.removeItem("cart");
-                window.location.href = `/accounts`;
+                // window.location.href = `/accounts`;
             }
         } catch (error) {
             console.error("Error submitting order:", error);
